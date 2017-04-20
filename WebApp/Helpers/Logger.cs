cda309b03432e74297860a8090737662016b2f2c
Krishan Aggarwal <kakaggarwal@gmail.com>
2017-04-20 18:57:59 +0530

    updated

--- WebApp/Helpers/Logger.cs
+++ WebApp/Helpers/Logger.cs
@@ -1,76 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
+using System.Web.Hosting;
 
 namespace WebApp.Helpers
 {
     public static class Logger
     {
-        private static string exceptionLogFileName = "";
-        private static string exceptionLogFileDirectory = "";
-        private static string activityLogFileName = "";
-        private static string activtyLogFileDirectory = "";
+        private static string exceptionLogFileName = System.Configuration.ConfigurationManager.AppSettings.Get("ExceptionLogFileName");
+        private static string exceptionLogFileDirectory = System.Configuration.ConfigurationManager.AppSettings.Get("ExceptionLogDirectory");
+        private static string activityLogFileName = System.Configuration.ConfigurationManager.AppSettings.Get("ActivityLogFileName");
+        private static string activtyLogFileDirectory = System.Configuration.ConfigurationManager.AppSettings.Get("ActivityLogDirectory");
 
-        public static string ExceptionLogFileName
-        {
-            get
-            {
-                if (exceptionLogFileName.Equals(""))
-                {
-                    exceptionLogFileName = System.Configuration.ConfigurationManager.AppSettings.Get("ExceptionLogFileName");
-                }
-
-                return exceptionLogFileName;
-            }
-        }
-
-        public static string ExceptionLogFileDirectory
-        {
-            get
-            {
-                if (exceptionLogFileDirectory.Equals(""))
-                {
-                    exceptionLogFileDirectory = System.Configuration.ConfigurationManager.AppSettings.Get("ExceptionLogDirectory");
-                }
-
-                return exceptionLogFileDirectory;
-            }
-        }
-
-        public static string ActivityLogFileName
-        {
-            get
-            {
-                if (activityLogFileName.Equals(""))
-                {
-                    activityLogFileName = System.Configuration.ConfigurationManager.AppSettings.Get("ActivityLogFileName");
-                }
-
-                return activityLogFileName;
-            }
-        }
-
-        public static string ActivityLogFileDirectory
-        {
-            get
-            {
-                if (activtyLogFileDirectory.Equals(""))
-                {
-                    activtyLogFileDirectory = System.Configuration.ConfigurationManager.AppSettings.Get("ActivityLogDirectory");
-                }
-
-                return activtyLogFileDirectory;
-            }
-        }
-
-        public static bool LogException()
+        public static bool LogException(Exception exception)
         {
             bool success = false;
 
             try
             {
+                string serverPath = HostingEnvironment.MapPath(exceptionLogFileDirectory);
 
+                if (Directory.Exists(serverPath))
+                {
+                    Directory.CreateDirectory(serverPath);
+                }
+
+                using (StreamWriter streamWriter = File.AppendText(serverPath + exceptionLogFileName))
+                {
+                    streamWriter.WriteLine("*************************** Log Entry Begin *************************************");
+                    streamWriter.WriteLine("Log Entry: " + DateTime.Now);
+                    streamWriter.WriteLine("Exception:");
+                    streamWriter.WriteLine("Message: " + exception.Message);
+                    streamWriter.WriteLine("Source: " + exception.Source);
+                    streamWriter.WriteLine("StackTrace: " + exception.StackTrace);
+
+                    exception = exception.InnerException;
+
+                    while (exception != null)
+                    {
+                        streamWriter.WriteLine("InnerException:");
+                        streamWriter.WriteLine("Message: " + exception.Message);
+                        streamWriter.WriteLine("Source: " + exception.Source);
+                        streamWriter.WriteLine("StackTrace: " + exception.StackTrace);
+
+                        exception = exception.InnerException;
+                    }
+
+                    streamWriter.WriteLine("*************************** Log Entry Ends *************************************");
+                }
             }
             catch(Exception ex)
             {
