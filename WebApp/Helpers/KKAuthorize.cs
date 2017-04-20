83c1b26dc475c2584f3700445b791d9f1ccdd5d5
Krishan Aggarwal <kakaggarwal@gmail.com>
2017-04-20 11:05:27 +0530

    Created Projected Adding to Git Source Control

---
+++ WebApp/Helpers/KKAuthorize.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Helpers
+{
+    public class KKAuthorizeAttribute : AuthorizeAttribute
+    {
+        public KKAuthorizeAttribute()
+        {
+
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+
+            try
+            {
+                // Authorization Logic
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            return authorize;
+        }
+    }
+}
---
+++ WebApp/Helpers/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Helpers
+{
+    public static class Logger
+    {
+        private string logFileName = "";
+        private string logFileDirectory = "";
+
+        public string LogFileName
+        {
+            get
+            {
+                if (logFileName.Equals(""))
+                {
+                    logFileName = System.Configuration.ConfigurationManager.AppSettings.Get("ExceptionLogFileName");
+                }
+
+                return logFileName;
+            }
+        }
+
+        public static bool LogException()
+        {
+            bool success = false;
+
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                success = false;
+                string fault = ex.Message;
+            }
+
+            return success;
+        }
+    }
+}
